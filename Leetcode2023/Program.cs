c6684d763e1e30ba9599e0f9ec67559788e8921d
anand9589 <anand9589@gmail.com>
2023-04-16 19:22:45 -0500

    clean program.cs

--- Leetcode2023/Program.cs
+++ Leetcode2023/Program.cs
@@ -1,10 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using April;
-using Common;
-//string[] arr = File.ReadAllLines(@"C:\Users\anand\source\repos\Leetcode2023\Leetcode2023\Testcase.txt");
-//int[] spells = Array.ConvertAll(arr[0].Trim('[', ']').Split(','), int.Parse);
-////int[] potions = Array.ConvertAll(arr[1].Trim('[', ']').Split(','), int.Parse);
-//int success = int.Parse(arr[1].Trim());
-Solution solution = new Solution();
-solution.NumEnclaves(Utilities.Get2DArray());
+
+
+Console.WriteLine("Hello World");
