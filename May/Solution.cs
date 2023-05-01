6b277bd11a2d87b112eb00ce41330308e5ec120f
anand9589 <anand9589@gmail.com>
2023-04-30 19:32:03 -0500

    Added Problem  1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ May/Solution.cs
@@ -0,0 +1,73 @@
+﻿namespace May
+{
+    public class Solution
+    {
+        #region Day 1 Problem  1491. Average Salary Excluding the Minimum and Maximum Salary
+        public double Average(int[] salary)
+        {
+
+            return (double)(salary.Sum() - salary.Max() - salary.Min()) / (salary.Length - 2);
+        }
+        #endregion
+        #region Day 2 Problem
+        #endregion
+        #region Day 3 Problem
+        #endregion
+        #region Day 4 Problem
+        #endregion
+        #region Day 5 Problem
+        #endregion
+        #region Day 6 Problem
+        #endregion
+        #region Day 7 Problem
+        #endregion
+        #region Day 8 Problem
+        #endregion
+        #region Day 9 Problem
+        #endregion
+        #region Day 10 Problem
+        #endregion
+        #region Day 11 Problem
+        #endregion
+        #region Day 12 Problem
+        #endregion
+        #region Day 13 Problem
+        #endregion
+        #region Day 14 Problem
+        #endregion
+        #region Day 15 Problem
+        #endregion
+        #region Day 16 Problem
+        #endregion
+        #region Day 17 Problem
+        #endregion
+        #region Day 18 Problem
+        #endregion
+        #region Day 19 Problem
+        #endregion
+        #region Day 20 Problem
+        #endregion
+        #region Day 21 Problem
+        #endregion
+        #region Day 22 Problem
+        #endregion
+        #region Day 23 Problem
+        #endregion
+        #region Day 24 Problem
+        #endregion
+        #region Day 25 Problem
+        #endregion
+        #region Day 26 Problem
+        #endregion
+        #region Day 27 Problem
+        #endregion
+        #region Day 28 Problem
+        #endregion
+        #region Day 29 Problem
+        #endregion
+        #region Day 30 Problem
+        #endregion
+        #region Day 31 Problem
+        #endregion
+    }
+}
