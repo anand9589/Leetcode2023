61c2c5f750f835f147232069cd171569b8834c98
anand9589 <anand9589@gmail.com>
2023-05-01 20:55:32 -0500

    Added Problem  1822. Sign of the Product of an Array

--- May/Solution.cs
+++ May/Solution.cs
@@ -3,7 +3,22 @@
     public class Solution
     {
         #region Day 1 Problem  1491. Average Salary Excluding the Minimum and Maximum Salary
+
         public double Average(int[] salary)
+        {
+            Array.Sort(salary);
+
+            int n = salary.Length;
+            int sum = 0;
+            for (int i = 1; i < n - 1; i++)
+            {
+                sum += salary[i];
+            }
+
+            return (double)sum / n - 2;
+        }
+
+        public double Average_V1(int[] salary)
         {
             int maxSalary = int.MinValue;
             int minSalary = int.MaxValue;
@@ -18,12 +33,40 @@
                 maxSalary = Math.Max(maxSalary, i);
             }
 
-            return (double)(salarySum - maxSalary - minSalary)/(n-2);
+            return (double)(salarySum - maxSalary - minSalary) / (n - 2);
             //return (double)(salary.Sum() - salary.Max() - salary.Min()) / (salary.Length - 2);
         }
         #endregion
-        #region Day 2 Problem
+
+        #region Day 2 Problem  1822. Sign of the Product of an Array
+        public int ArraySign(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int low = 0;
+
+            int high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (nums[mid] == 0) return 0;
+
+                if (nums[mid] > 0)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return low % 2 == 0 ? 1 : -1;
+        }
         #endregion
+
         #region Day 3 Problem
         #endregion
         #region Day 4 Problem
