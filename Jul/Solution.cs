62ffb2a3cd21b68bcc57f0871515425fb253f433
anand9589 <anand9589@gmail.com>
2023-06-30 22:00:36 -0500

    2305. Fair Distribution of Cookies

--- Jul/Solution.cs
+++ Jul/Solution.cs
@@ -2,6 +2,43 @@
 {
     public class Solution
     {
+        #region Day 1 Problem 2305. Fair Distribution of Cookies
+        public int DistributeCookies(int[] cookies, int k)
+        {
+            int[] distribution = new int[k];
 
+            return dfs_2305(0, distribution, cookies, k, k);
+        }
+
+        private int dfs_2305(int i, int[] distribution, int[] cookies, int k, int zero)
+        {
+            if (cookies.Length - i < zero)
+            {
+                return int.MaxValue;
+            }
+
+            if (i == cookies.Length)
+            {
+                return distribution.Max();
+            }
+
+            int result = int.MaxValue;
+
+            for (int j = 0; j < k; j++)
+            {
+                zero -= distribution[j] == 0 ? 1 : 0;
+                distribution[j] += cookies[i];
+
+                result = Math.Min(result, dfs_2305(i + 1, distribution, cookies, k, zero));
+
+                distribution[j] -= cookies[i];
+
+                zero += distribution[j] == 0 ? 1 : 0;
+            }
+
+            return result;
+
+        }
+        #endregion
     }
 }
