4f7218fa7ba12019cc393196b4647338399d9ac8
anand9589 <anand9589@gmail.com>
2023-08-31 19:31:22 -0500

    Added Problem 338. Counting Bits

--- Sep/Solution.cs
+++ Sep/Solution.cs
@@ -2,6 +2,26 @@
 {
     public class Solution
     {
+        #region Day 1  Problem 338. Counting Bits
+        public int[] CountBits(int n)
+        {
+            int[] result = new int[n + 1];
+            result[0] = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int y = i / 2;
 
+
+                result[i] = result[y];
+
+                if (i % 2 == 1)
+                {
+                    result[i]++;
+                }
+            }
+
+            return result;
+        }
+        #endregion
     }
 }
